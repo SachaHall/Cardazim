c946c022dca123c65b13d59d0f32fefbd3a2ed4f
Sacharlemagne <sacha.hallermeier@gmail.com>
2024-09-17 13:49:02 +0300

    remove file

--- networking/pita.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import AES
-import hashlib
-key = b'Sixtfrfrfreen byte keyde'
-en_key = hashlib.sha256(key).digest()
-plaintext = b'hello worljjjjfd'
-cipher = AES.new(en_key, AES.MODE_EAX, nonce=b'arazim')
-encrypted = cipher.encrypt(plaintext)
-print(len(encrypted) == len(plaintext))
